88c0b63cf361cae6d8932e6ce5942674816a4002
amdoulabdallah7-dot <amdoulabdallah7@gmail.com>
2026-06-15 13:20:38 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -30,7 +30,8 @@ const routes = [
 ]   
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
+  base: import.meta.env.BASE_URL,
   routes
 })
 

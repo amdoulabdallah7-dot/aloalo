6f7fc13c72b3221faffdf7f5a6929e5153203497
amdoulabdallah7-dot <amdoulabdallah7@gmail.com>
2026-06-15 13:32:21 +0300

    aloalo

--- src/router/index.js
+++ src/router/index.js
@@ -30,8 +30,7 @@ const routes = [
 ]   
 
 const router = createRouter({
-  history: createWebHistory(),
-  base: import.meta.env.BASE_URL,
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
